34349821ef60fb5bf3eb52356da0270deda68b68
Chris Eidhof <chris@eidhof.nl>
2019-08-13 16:15:21 +0200

    Starting point

--- ParseCSVTests/ParseCSVTests.swift
+++ ParseCSVTests/ParseCSVTests.swift
@@ -1,60 +1,5 @@
 import XCTest
 
-extension Substring {
-    mutating func remove(upToAndIncluding idx: Index) {
-        self = self[index(after: idx)...]
-    }
-    
-    mutating func parseField() -> Substring {
-        assert(!self.isEmpty)
-        switch self[startIndex] {
-        case "\"":
-            removeFirst()
-            guard let quoteIdx = index(of: "\"") else {
-                fatalError("expected quote") // todo throws
-            }
-            let result = prefix(upTo: quoteIdx)
-            remove(upToAndIncluding: quoteIdx)
-            if !isEmpty {
-                let comma = removeFirst()
-                assert(comma == ",") // todo throws
-            }
-            return result
-            
-        default:
-            if let commaIdx = index(of: ",") {
-                let result = prefix(upTo: commaIdx)
-                remove(upToAndIncluding: commaIdx)
-                return result
-            } else {
-                let result = self
-                removeAll()
-                return result
-            }
-        }
-    }
-}
-
-func parse(line: Substring) -> [Substring] {
-    var remainder = line
-    var result: [Substring] = []
-    while !remainder.isEmpty {
-        result.append(remainder.parseField())
-    }
-    return result
-}
-
-func parse(lines: String) -> [[Substring]] {
-    return lines.split(whereSeparator: { char in
-        switch char {
-        case "\r", "\n", "\r\n": return true
-        default: return false
-        }
-    }).map { line in
-        parse(line: line)
-    }
-}
-
 extension String {
     func parseAlt() -> [[String]] {
         var result: [[String]] = [[]]
@@ -94,19 +39,9 @@ class ParseCSVTests: XCTestCase {
     let cases: [CSVTestCase] = [
         CSVTestCase(name: "line", input: "one,2,,three", expected: [["one", "2", "", "three"]]),
         CSVTestCase(name: "multipleLines", input: "one,2,,three\nfive,six,\"hello,q\"", expected: [["one", "2", "", "three"], ["five", "six", "hello,q"]]),
-        CSVTestCase(name: "quotes", input: "one,\"qu,ote\",2,,three", expected: [["one", "qu,ote", "2", "", "three"]]),
-        CSVTestCase(name: "multiline", input: "one,2,,three\nfour,five", expected: [["one", "2", "", "three"], ["four","five"]]),
-//        CSVTestCase(name: "crlf", input: "one,2,,three\r\nfour,five", expected: [["one", "2", "", "three"], ["four","five"]])
-        
+        CSVTestCase(name: "quotes", input: "one,\"qu,ote\",2,,three", expected: [["one", "qu,ote", "2", "", "three"]]),        
     ]
-    
-    func testParse1() {
-        for c in cases {
-            let result = parse(lines: c.input).map { $0.map(String.init) }
-            XCTAssertEqual(result, c.expected, "Case \(c.name) failed")
-        }
-    }
-    
+        
     func testParseAlt() {
         for c in cases {
             let result = c.input.parseAlt()
